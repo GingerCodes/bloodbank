51224e232fc80a70ab0fe1bf022d896d9b210229
Alex Jose <alexjose4u@gmail.com>
2015-07-17 00:04:13 +0530

    Request Model Created

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,5 +1,18 @@
-app.controller("homepagectrl",function($scope,Profile){
-    $scope.users = Profile.find({filter:{where:{first_name:'Alex'}}});
+app.controller("homepagectrl", function ($scope, Profile, Request) {
+    $scope.users = Profile.find({filter: {where: {first_name: 'Alex'}}});
+    $scope.requests = Request.find();
     //$scope.users = Profile.count();
     console.log($scope.users);
+    console.log($scope.requests);
+
+
+    $scope.submit = function () {
+        console.log('Submitted');
+        var request = new Request();
+        console.log(request);
+        request.blood_group = $scope.blood_group;
+        request.location = $scope.location;
+        request.$save();
+        $scope.requests = Request.find();
+    }
 });
