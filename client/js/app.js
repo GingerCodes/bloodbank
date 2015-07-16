2b47c897ce1f232a528ed55fc772e57d5688a6af
Thomas Paul <thomaspaul59@gmail.com>
2015-07-16 19:19:11 +0530

    Resource tested

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,12 @@
-var app=angular.module("bloodbankapp",['ngRoute','ngResource','ui.bootstrap']);
+var app=angular.module("bloodbankapp",['ngRoute','ngResource','ui.bootstrap', 'lbServices']);
+
+app
+ .config(function($routeProvider, $locationProvider){
+  $routeProvider
+   .when('/', {
+    controller: 'homepagectrl',
+    templateUrl: 'views/home.html'
+   });
+
+  $locationProvider.html5Mode(true);
+ });
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,3 +1,5 @@
-app.controller("homepagectrl",function($scope,User){
-    $scope.users = User.query();
+app.controller("homepagectrl",function($scope,Profile){
+    $scope.users = Profile.find({filter:{where:{first_name:'Alex'}}});
+    //$scope.users = Profile.count();
+    console.log($scope.users);
 });
