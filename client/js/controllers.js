f679e6dce0882b492b535343a2bd3bc77c83186e
Alex Jose <alexjose4u@gmail.com>
2015-07-19 23:13:11 +0530

    User Registration Form Completed

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,9 +1,40 @@
-app.controller("homepagectrl", function ($scope, Profile, Request) {
-    $scope.users = Profile.find({filter: {where: {first_name: 'Alex'}}});
-    $scope.requests = Request.find();
-    //$scope.users = Profile.count();
-    console.log($scope.users);
-    console.log($scope.requests);
+app.controller("HomeController", function ($scope, $q, Profile, Request, User) {
+    $scope.users = User.count();
+
+    $scope.blood_groups = [
+        'A+',
+        'A-',
+        'B+',
+        'B-',
+        'AB+',
+        'AB-',
+        'O+',
+        'O-',
+        'Any',
+    ];
+
+    $scope.location_options = {
+        types: '(cities)',
+        watchEnter: true
+    };
+
+    $scope.signup = function () {
+        if ($scope.user.$valid) {
+            var user = new User();
+            user.email = $scope.user.email;
+            user.password = $scope.user.password;
+            user.first_name = $scope.user.first_name;
+            user.last_name = $scope.user.last_name;
+            user.blood_group = $scope.user.blood_group;
+            user.location = $scope.user.location;
+            user.location_details = $scope.user.location_details;
+            user.date_of_birth = "2-2-1988";
+            user.gender = true;
+            user.$create();
+        } else {
+            console.log("Not Valid");
+        }
+    }
 
 
     $scope.submit = function () {
