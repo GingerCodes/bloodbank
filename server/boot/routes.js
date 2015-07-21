fa0b9ecb248a0e109d7113a1c8ac5cdd60065eeb
Alex Jose <alexjose4u@gmail.com>
2015-07-21 16:06:26 +0530

    Express Routing Update
    Express routing will now forward all the request to angular, so that
    angular app can handle the routes.

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,7 +1,10 @@
-module.exports = function(app) {
-  var router = app.loopback.Router();
-  router.get('/ping', function(req, res) {
-    res.send('pong');
-  });
-  app.use(router);
+module.exports = function (app) {
+    var router = app.loopback.Router();
+    router.get('/ping', function (req, res) {
+        res.send('pong');
+    });
+    router.get('/*', function (req, res) {
+        res.sendFile('index.html', {root: 'client'});
+    });
+    app.use(router);
 }
--- server/server.js
+++ server/server.js
@@ -1,22 +1,28 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var path = require('path');
 
 var app = module.exports = loopback();
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    console.log('Web server listening at: %s', app.get('url'));
-  });
+app.use(loopback.static(path.resolve(__dirname, '../client/')));
+
+app.start = function () {
+    // start the web server
+    return app.listen(function () {
+        app.emit('started');
+        console.log('Web server listening at: %s', app.get('url'));
+    });
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
+boot(app, __dirname, function (err) {
+    if (err)
+        throw err;
 
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+    // start the server if `$ node server.js`
+    if (require.main === module)
+        app.start();
 });
+console.log("This");
+
