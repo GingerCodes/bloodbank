f57203e304bfd3e8c8b1fe2f72122c4ef8c91e7b
Alex Jose <alexjose4u@gmail.com>
2015-07-24 19:12:32 +0530

    user and request model updated
    user model is updated with new acl and new method. request model is
    updated with new acl.

--- common/models/user.js
+++ common/models/user.js
@@ -1,3 +1,34 @@
-module.exports = function(User) {
+module.exports = function (User) {
 
+    User.countDonors = function (blood_group, location, cb) {
+
+        var diff = 0.03
+
+        var upper_lat = location.A + diff;
+        var lower_lat = location.A - diff;
+        var upper_lng = location.F + diff;
+        var lower_lng = location.F - diff;
+
+        User.count({
+            "blood_group": blood_group,
+            "location_details.geometry.location.A": {between: [lower_lat, upper_lat]},
+            "location_details.geometry.location.F": {between: [lower_lng, upper_lng]}
+        }, function (err, count) {
+            if (err)
+                throw err
+            cb(null, count);
+        });
+    }
+
+    User.remoteMethod(
+            'countDonors',
+            {
+                accepts: [
+                    {arg: 'blood_group', type: 'string', required: true},
+                    {arg: 'location', type: 'object', required: true}
+                ],
+                returns: {arg: 'count', type: 'number'},
+                description: "Returns the count of donors available for the given blood group in the provided location.",
+            }
+    );
 };
