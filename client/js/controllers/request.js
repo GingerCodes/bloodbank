c4cc83ada46559fcb69a26d1848a4b5b3f1af40b
Alex Jose <alexjose4u@gmail.com>
2015-07-29 10:14:54 +0530

    Confirmation on Donation
    Donate button will be disabled and the confirmation message will be
    displayed. Closes #7.

--- client/js/controllers/request.js
+++ client/js/controllers/request.js
@@ -4,7 +4,7 @@ app.controller('RequestController', ['$scope', 'request', 'user', 'is_donated', 
         $scope.user = user;
         $scope.is_donated = is_donated;
         $scope.count = count;
-        
+
 
 
         $scope.donate = function () {
@@ -13,6 +13,9 @@ app.controller('RequestController', ['$scope', 'request', 'user', 'is_donated', 
                 id: user.id
             }, {
                 is_confirm: true
+            }).$promise.then(function () {
+                $scope.is_donated = true;
+                Materialize.toast("Recieved you interest. Thank You.", 4000);
             });
         }
     }
